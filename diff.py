7848baff536f035380a3862a5f3e31be017eabb2
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-08-20 14:01:51 +0200

    Add multiline diff function

---
+++ diff.py
@@ -0,0 +1,58 @@
+def get_lcs(s1, s2):
+    ROWS, COLUMNS = len(s2), len(s1)
+    if ROWS == 0 or COLUMNS == 0:
+        return ""
+    dp = [[""] * (COLUMNS + 1) for _ in range(ROWS + 1)]
+    for row in range(1, ROWS + 1):
+        for col in range(1, COLUMNS + 1):
+            if s1[col - 1] == s2[row - 1]:
+                dp[row][col] = dp[row - 1][col - 1] + s1[col - 1]
+            else:
+                dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+    return dp[ROWS][COLUMNS]
+
+
+def get_lcs_multiline(s1: list[str], s2: list[str]) -> list[str]:
+    ROWS, COLUMNS = len(s2), len(s1)
+    if ROWS == 0 or COLUMNS == 0:
+        return []
+    dp = [[[] for _ in range(COLUMNS + 1)] for _ in range(ROWS + 1)]
+    for row in range(1, ROWS + 1):
+        for col in range(1, COLUMNS + 1):
+            if s1[col - 1] == s2[row - 1]:
+                dp[row][col] = dp[row - 1][col - 1] + [s1[col - 1]]
+            else:
+                dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+    return dp[ROWS][COLUMNS]
+
+
+def _get_lcs_indexes(s1: list[str], s2: list[str]) -> list[tuple[int, int]]:
+    ROWS, COLUMNS = len(s2), len(s1)
+    if ROWS == 0 or COLUMNS == 0:
+        return []
+    dp = [[[] for _ in range(COLUMNS + 1)] for _ in range(ROWS + 1)]
+    for row in range(1, ROWS + 1):
+        for col in range(1, COLUMNS + 1):
+            if s1[col - 1] == s2[row - 1]:
+                dp[row][col] = dp[row - 1][col - 1] + [(col - 1, row - 1)]
+            else:
+                dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+    return dp[ROWS][COLUMNS]
+
+
+def get_diff(s1: list[str], s2: list[str]) -> tuple[list[str], list[str]]:
+    indexes = _get_lcs_indexes(s1, s2)
+    s1_common_indexes = {i[0] for i in indexes}
+    s2_common_indexes = {i[1] for i in indexes}
+    s1_diff = [s for idx, s in enumerate(s1) if idx not in s1_common_indexes]
+    s2_diff = [s for idx, s in enumerate(s2) if idx not in s2_common_indexes]
+    return (s1_diff, s2_diff)
+
+
+def print_diff(s1: list[str], s2: list[str]) -> None:
+    s1_diff, s2_diff = get_diff(s1, s2)
+    for s in s1_diff:
+        print(f"< {s}")
+    print("---")
+    for s in s2_diff:
+        print(f"> {s}")
---
+++ test_diff.py
@@ -0,0 +1,75 @@
+import pytest
+from diff.diff import get_lcs, get_lcs_multiline, get_diff
+
+
+@pytest.mark.parametrize(
+    "s1, s2, result",
+    [
+        ("ABCDEF", "ABCDEF", "ABCDEF"),
+        ("ABC", "XYZ", ""),
+        ("AABCXY", "XY", "XY"),
+        ("", "", ""),
+        ("abcdrau", "axdlau", "adau"),
+    ],
+)
+def test_get_lcs(s1, s2, result):
+    assert get_lcs(s1, s2) == result
+
+
+@pytest.mark.parametrize(
+    "lines1, lines2, result",
+    [
+        (
+            ["my name is alex", "i'm a developer", "i'm 31 yo"],
+            ["my name is alexander", "i'm a developer", "i'm 30 yo"],
+            ["i'm a developer"],
+        ),
+        (
+            [],
+            [
+                "my name is alexander",
+            ],
+            [],
+        ),
+        (
+            [],
+            [],
+            [],
+        ),
+    ],
+)
+def test_get_lcs_multilines(lines1, lines2, result):
+    act_result = get_lcs_multiline(lines1, lines2)
+    assert act_result == result
+
+
+@pytest.mark.parametrize(
+    "lines1, lines2, result",
+    [
+        (
+            ["my name is alex", "i'm a developer", "i'm 31 yo"],
+            ["my name is alexander", "i'm a developer", "i'm 30 yo"],
+            (["my name is alex", "i'm 31 yo"], ["my name is alexander", "i'm 30 yo"]),
+        ),
+        (
+            [],
+            [
+                "my name is alexander",
+            ],
+            ([], ["my name is alexander"]),
+        ),
+        (
+            [],
+            [],
+            ([], []),
+        ),
+        (
+            ["a", "b", "c"],
+            ["b", "d"],
+            (["a", "c"], ["d"]),
+        ),
+    ],
+)
+def test_get_diff(lines1, lines2, result):
+    act_result = get_diff(lines1, lines2)
+    assert act_result == result
